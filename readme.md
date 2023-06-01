57e358f050b3c5fa87c43334b754768ccf2fc519
amarjeetk123 <ak123del@gmail.com>
2023-05-31 22:36:45 -0600

    adding a readmefile

---
+++ readme.md
@@ -0,0 +1,11 @@
+ # ToDo App
+
+![](https://img.shields.io/badge/iNeuron-orange)
+![](https://img.shields.io/badge/Hitesh%20Chaoudhry-LCO-g)
+
+### Technology Used
+  - ` React `
+
+Live Link:- [Click Here!](https://placement-assignment-amarjeet-kumar-todo-app.vercel.app/)
+
+![](./web.png)
--- src/components/todo.js
+++ src/components/todo.js
@@ -3,16 +3,16 @@ import "../App.css"
 import { useReducer } from "react";
 
 const initialSate = []
-function reducer(state,action) {
-  switch(action.type){
+function reducer(state, action) {
+  switch (action.type) {
     case "ADD_TASK": return [
       ...state,
       {
-        id:state.length+1,
-        name:action.payload
+        id: state.length + 1,
+        name: action.payload
       }
     ]
-break;
+      break;
     default: return state;
   }
 
@@ -20,17 +20,17 @@ break;
 
 const Todo = () => {
 
-  const[todos, dispatch] = useReducer( reducer ,initialSate);
+  const [todos, dispatch] = useReducer(reducer, initialSate);
   const [userInput, setUserInput] = useState("");
 
   const handleClick = () => {
-  if(userInput!==""){
-    dispatch({type:'ADD_TASK'  , payload:userInput});
-setUserInput("")
-  }
-  else{
-    alert("Please Write Something in the Box.")
-  }
+    if (userInput !== "") {
+      dispatch({ type: 'ADD_TASK', payload: userInput });
+      setUserInput("")
+    }
+    else {
+      alert("Please Write Something in the Box.")
+    }
   }
 
 
@@ -39,17 +39,17 @@ setUserInput("")
       <div className='main'>
         <center>ToDO-List</center>
         <div>
-          <input className='top-box' placeholder='Please add your task.....' value={userInput} onChange={(e) => setUserInput(e.target.value)}  />
+          <input className='top-box' placeholder='Please add your task.....' value={userInput} onChange={(e) => setUserInput(e.target.value)} />
           <button className='add-btn' onClick={() => {
             handleClick();
-         
+
           }}  >   Add Todo</button>
         </div>
         <div className='containt'>
           {
-            todos.map(todos => <h2 className='todo' key={todos.id} > {todos.id}. {todos.name} </h2> )
-          }  
-          
+            todos.map(todos => <h2 className='todo task-done' key={todos.id} > {todos.id}. {todos.name} </h2>)
+          }
+
           <div>
             {/* <button className='delete-btn'>Delete</button> */}
           </div>
